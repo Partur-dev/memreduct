928ff5dca93768796670e6a215426154041482ee
Partur-dev <parturtwo@gmail.com>
2022-01-13 00:39:14 +0200

    лiнух

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-#include <Windows.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define sleep(ms) Sleep(ms);
+#else
+#include <unistd.h>
+#define sleep(ms) usleep(ms*1000);
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -9,7 +16,7 @@ int main(int argc, char *argv[])
     char *mem = malloc(size*sizeof(char));
     memset(mem, '\0', size*sizeof(char));
 
-    Sleep(time*1000);
+    sleep(time*1000);
     free(mem);
 
     return 0;
